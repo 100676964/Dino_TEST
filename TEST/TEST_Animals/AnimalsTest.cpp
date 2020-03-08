760aaa47def5f99a07423d9fbeecf806f0c98e80
100676964 <48954934+100676964@users.noreply.github.com>
2020-03-08 18:33:18 -0400

    Add files via upload

--- TEST/TEST_Animals/AnimalsTest.cpp
+++ TEST/TEST_Animals/AnimalsTest.cpp
@@ -1,25 +1,32 @@
-#include "../../Animal.h"
+#include "../../Animals.h"
 using namespace Dino;
-bool test_returnX()
+bool test_updateAnimals()
 {
     Animal bird(1,10,10,10);
     std::cout<<bird.returnX();
 
 }
-bool test_returnY()
+bool test_reset()
 {
     Animal bird(1,10,10,10);
     std::cout<<bird.returnY();
 }
-bool test_returnFrame()
+bool test_autoAnimateAnimals()
 {
 
 }
-bool test_setPos()
+bool test_stop()
 {
 
 }
+bool test_continueAll()
+{
 
+}
+bool test_returnPassingSpeed()
+{
+
+}
 int main()
 {
     
