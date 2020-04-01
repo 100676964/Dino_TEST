519026924eadde52bd248eeb31d509653af26f75
100676964 <48954934+100676964@users.noreply.github.com>
2020-04-01 14:31:31 -0400

    Add files via upload

---
+++ TEST/UNIT_TESTs/TEST_Obstacle/ObstacleTest.cpp
@@ -0,0 +1,26 @@
+#include ../../../Animal.h"
+using namespace Dino;
+bool test_returnX()
+{
+    Animal bird(1,10,10,10);
+    std::cout<<bird.returnX();
+
+}
+bool test_returnY()
+{
+    Animal bird(1,10,10,10);
+    std::cout<<bird.returnY();
+}
+bool test_returnFrame()
+{
+
+}
+bool test_setPos()
+{
+
+}
+
+int main()
+{
+    
+}
