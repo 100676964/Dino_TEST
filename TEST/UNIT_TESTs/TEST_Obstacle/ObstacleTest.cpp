295ba270d4d07ecbbb905c42c2e4fbcbe96831d8
Weimin Zhao 100588860 <32662816+WeiminZhao@users.noreply.github.com>
2020-04-01 15:13:34 -0400

    Update Obstacles.cpp
    Actually write some testing functions.

--- TEST/UNIT_TESTs/TEST_Obstacle/ObstacleTest.cpp
+++ TEST/UNIT_TESTs/TEST_Obstacle/ObstacleTest.cpp
@@ -1,32 +1,60 @@
 #include "../../../Obstacle.h"
 using namespace Dino;
-bool test_returnX()
+bool test_returnSizeX()
 {
     Obstacle ob(1);
-    std::cout<<ob.returnX();
-
+    int val=ob.returnSizeX();
+    if(val!=9) return false;
+    
+    Obstacle ob2(2);
+    int val=ob2.returnSizeX();
+    if(val!=12) return false;
+    
+    Obstacle ob3(-1);
+    int val=ob3.returnSizeX();
+    if(val!=6) return false;
+    
+    return true;
 }
-bool test_returnY()
+bool test_returnSizeY()
 {
     Obstacle ob(1);
-    std::cout<<ob.returnY();
+    int val=ob.returnSizeY();
+    if(val!=12) return false;
+    
+    Obstacle ob2(2);
+    int val=ob2.returnSizeY();
+    if(val!=23) return false;
+    
+    Obstacle ob3(-1);
+    int val=ob3.returnSizeY();
+    if(val!=6) return false;
+    
+    return true;
 }
 
 bool test_setPos()
 {
     Obstacle ob(1);
     ob.setPos(2,5);
-    std::cout<<ob.returnX();
-    std::cout<<ob.returnY();
+    if(ob.returnPosX()!=2) return false;
+    if(ob.returnPosY()!=5) return false;
+    
+    return true;
 }
 bool test_collipse()
 {
     Obstacle ob(1);
-    std::cout<<ob.collipsed();
-    ob.isCollipsed();
-    std::cout<<ob.collipsed();
+    if(ob.isCollipsed()!=false) return false;
+        
+    ob.collipsed();
+    if(ob.isCollipsed()!=true) return false;
+    
+    return true;
 }
 int main()
 {
-
+    if(test_returnSizeX()&&test_returnSizeY()&&test_setPos()&&test_collipse()) std::cout<<"Obstacle test: passed\n";
+    else  std::cout<<"Obstacle test: failed\n";
+    
 }
