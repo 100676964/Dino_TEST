a41970e5f5377f094ea6cce78abe9e1b14620528
100676964 <48954934+100676964@users.noreply.github.com>
2020-04-02 17:26:24 -0400

    Add files via upload

---
+++ TEST/UNIT_TESTs/TEST_Dino/DinoTest.h
@@ -0,0 +1,19 @@
+#include "../../../Dino.h"
+using namespace Dino;
+bool test_reset();
+bool test_setStretch();
+bool test_returnDino();
+bool test_returnCurrentFrame();
+bool test_increaseFrame();
+bool test_decreaseFrame();
+bool test_setDead();
+bool test_deadOrNot();
+bool test_autoFrameChange();
+bool test_setJump();
+bool test_calculateGravity();
+bool test_calculateCollipse();
+bool test_calculateHp();
+bool test_returnYSize();
+bool test_returnXSize();
+bool test_returnHScore();
+bool test_setHScore();
