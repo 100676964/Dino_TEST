81ae01d09a867d9bcf2b422d21571b1677f72d85
Weimin Zhao 100588860 <32662816+WeiminZhao@users.noreply.github.com>
2020-04-02 16:10:05 -0400

    Update AnimalsTest.cpp
    Hope it works

--- TEST/UNIT_TESTs/TEST_Animals/AnimalsTest.cpp
+++ TEST/UNIT_TESTs/TEST_Animals/AnimalsTest.cpp
@@ -1,33 +1,53 @@
 #include "../../Animals.h"
+#include "../../Environment.h"
 using namespace Dino;
-bool test_updateAnimals()
+bool test_initAnimals()
 {
-    Animal bird(1,10,10,10);
-    std::cout<<bird.returnX();
+    Animals tanys(10,10,1);
+    if(tanys.returnPassingSpeed!=2) return false;
+    
+    return true;
 
 }
 bool test_reset()
 {
-    Animal bird(1,10,10,10);
-    std::cout<<bird.returnY();
+    Animals tanys(10,10,1);
+    
+    tanys.setSpeed(50);
+    tanys.reset();
+    if(tanys.returnPassingSpeed!=2) return false;
+    
+    return true;
 }
-bool test_autoAnimateAnimals()
-{
 
+bool test_setPassingSpeed()
+{
+    Animals tanys(10,10,1);
+    
+    tanys.setSpeed(40);
+    if(tanys.returnPassingSpeed!=40) return false;
+    
+    return true;
 }
-bool test_stop()
+bool test_Update()
 {
-
-}
-bool test_continueAll()
-{
-
-}
-bool test_returnPassingSpeed()
-{
-
+    Environment en(10,10,1);
+    Animals tanys(10,10,1);
+    
+    en.setGroundPos(100);
+    
+    int index=0;
+    while(true)
+    {
+        tanys.updateAnimals(en);
+        
+        if(tanys.returnAnims().size()!=0) return true;
+        if(index>100000) return false;
+        index++;
+    }
 }
 int main()
 {
-
+    if(test_initAnimals()&&test_reset()&&test_setPassingSpeed()&&test_Update()) std::count<<"Animals Test: passed\n";
+    else std::count<<"Animals Test: failed\n";
 } 
