d0b5d460132bd58d42a6037eb1638c124f0e1c30
100676964 <48954934+100676964@users.noreply.github.com>
2020-04-03 22:01:19 -0400

    Add files via upload

--- TEST/Benchmark_TEST/DinoBenchmark.cpp
+++ TEST/Benchmark_TEST/DinoBenchmark.cpp
@@ -6,12 +6,11 @@
 #include <nana/threads/pool.hpp>
 #include <thread>
 #include <fstream>
-#include <mutex>
 #include "../../Dino.h"
 const int windowSizeX=800;
 const int windowSizeY=600;
 const int pixelStretch=4;
-mutex mtx;
+vector<int> fpss;
 void bench_marking()
 {
     
@@ -28,75 +27,102 @@ void bench_marking()
 
     form fm{API::make_center(windowSizeX, windowSizeY)};
     fm.caption(L"Bench Mark Test");
+    
+    
     drawing dw(fm);
+    drawing dw2(fm);
+    drawing dw3(fm);
     //paint
 	dw.draw([&envir, &dino, &anys, &fpstime,&fps,&printfps](paint::graphics &graph) 
     {//here we preset the method about all elements need to be paint, and set all the variable to control the paint process
 		
         graph.rectangle({0,0,windowSizeX,windowSizeY},true,colors::bisque);//set the background color
 		//progress drawing
-		envir.drawEnvironment(graph);
-		anys.autoAnimateAnimals(graph);
+		envir.drawGround(graph);
+        
+		
 		dino.autoAnimateDino(graph);
         
 	});
-    //physics loop
-    std::thread T([&]
+    dw2.draw([&envir, &dino, &anys, &fpstime,&fps,&printfps](paint::graphics &graph) 
     {
-        while(!quit)
+        envir.drawObstacle(graph);
+    });
+    dw3.draw([&envir, &dino, &anys, &fpstime,&fps,&printfps](paint::graphics &graph) 
+    {
+        anys.autoAnimateAnimals(graph);
+    });
+    //physics loop  
+    int duration = 0;
+    std::thread T([&]
+    {   
+        while(!quit && duration < 1000)
         {
-            std::thread PHY([&]
-            {   
-                if(chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now()-cfps).count()>=16)//update control for calculations/rendering, 16ms ~= 60 updates/sec
-                {
-                    mtx.lock();
-                    if(chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now()-fpstime).count()>=(1000))
-                    {
-                        cout<<printfps<<endl;
-                        fpstime=chrono::steady_clock::now();//chrono::system_clock::to_time_t(chrono::system_clock::now());
-                        printfps=fps;
-                        fps=0;
-                        
-                    }
-                    cfps=chrono::steady_clock::now();
-                    // dino.reset();
-                    envir.updateEnvironment();
-                    dino.updateDino(envir,anys,false);
-                    anys.updateAnimals(envir);
-                    fps++;
-                    mtx.unlock();
-                }
-            });
-            std::thread RF([&]
+            if(chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now()-cfps).count()>=16)//update control for calculations/rendering, 16ms ~= 60 updates/sec
             {
+                if(chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now()-fpstime).count()>=(1000))
+                {
+                    cout<<"current FPS: "<<printfps<<endl;
+                    fpstime=chrono::steady_clock::now();//chrono::system_clock::to_time_t(chrono::system_clock::now());
+                    printfps=fps;
+                    fps=0;
+                    fpss.push_back(printfps);
+                    
+                }
+                cfps=chrono::steady_clock::now();
+                dino.reset();
+                envir.updateEnvironment();
+                dino.updateDino(envir,anys,false);
+                anys.updateAnimals(envir);
                 API::refresh_window(fm);
-            });   
-            PHY.join();
-            RF.join();
+                fps++;
+                duration++;
+            }
         }
-    
-    });  
-    
+        fm.close();
+    });
     fm.events().unload([&quit](const arg_unload &arg)
     { 
         quit = true;    
     });
-    fm.events().key_press([&dino,&envir,&anys](const arg_keyboard& ak)
-    {
-		if(ak.key==nana::keyboard::space)
-		{
-            dino.setJump();
-        }
-    });
     dw.update();
     fm.show();
-    exec();
+    nana::exec();
     T.join();
-    
     
 }
 int main()
 {
-    bench_marking();
     
+    cout<<"Benchmarking!"<<endl; 
+    bench_marking();
+    cout<<"Test End!"<<endl;
+    double fpssum = 0;
+    int maxfps = 0;
+    int minfps = 1000;
+    for(int i = 2; i < fpss.size(); i++)
+    {
+        fpssum += fpss.at(i);
+        if(maxfps < fpss.at(i))
+        {
+            maxfps = fpss.at(i);
+        }
+        if(minfps > fpss.at(i))
+        {
+            minfps = fpss.at(i);
+        }
+    }
+    double avfps = fpssum/fpss.size();
+    cout<<"Average FPS: "<<avfps<<endl;
+    cout<<"Max FPS: "<<maxfps<<endl;
+    cout<<"Min FPS: "<<minfps<<endl;
+    ofstream file;
+    file.open("../TEST_Results/Benchmark_Results");
+    file<<"Average FPS: "<<endl;
+    file<<avfps<<endl;
+    file<<"Max FPS: "<<endl;
+    file<<maxfps<<endl;
+    file<<"Min FPS: "<<endl;
+    file<<minfps<<endl;
+    file.close();
 }
