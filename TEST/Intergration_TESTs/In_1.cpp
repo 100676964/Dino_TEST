b4c2f0e9bcb6c236750db4704935593c5f1fee5f
Weimin Zhao 100588860 <32662816+WeiminZhao@users.noreply.github.com>
2020-04-04 02:59:55 -0400

    Update In_1.cpp

--- TEST/Intergration_TESTs/In_1.cpp
+++ TEST/Intergration_TESTs/In_1.cpp
@@ -107,6 +107,93 @@ bool doIT::Dino_HP_Add()
         cout<<"crashed!"<<endl;
     }
 }
+
+
+
+bool doIT::Dino_Hit_Anims()//test animal(bird) collapse
+{
+    try
+    {
+        using namespace nana;
+        Environment env(windowSizeX,windowSizeY,5);
+        Dino::Dino dino(windowSizeX,windowSizeY,5);
+        Animals bird(windowSizeX,windowSizeY,5);
+        env.setGroundPos(env.returnYSize()*3/4+env.returnYSize()*pixelStretch/2);
+        form fm{API::make_center(windowSizeX, windowSizeY)};
+        fm.caption(L"Bench Mark Test");
+        drawing dw(fm);
+        dw.draw([&env,&dino](paint::graphics &graph) 
+        {
+            dino.autoAnimateDino(graph);
+            env.drawGround(graph);
+            env.drawObstacle(graph);
+            dino.displayHP(graph,windowSizeX-25,20,fnthp);
+        });
+        std::thread T([&]
+        { 
+            srand(0);
+            
+            int life = 5;
+            while(life > 0)
+            {
+                while(true)
+                {
+                    bird.updateAnimals(env);
+                    if(!bird.returnAnims().empty())
+                    {
+                        if(bird.returnAnims().at(0).type() == 1)
+                        {//we need to place the bird in right place to make the hit happens
+                            while(bird.returnAnims().at(0).returnX()>=1)//here probably will need a change to the number to make the hit
+                            {
+                                bird.updateAnimals(env);
+                            }
+                            break;
+                        }
+                    
+                    }
+                    // API::refresh_window(fm);
+                }
+                if(bird.returnAnims().size() == 1)
+                {   //update dino to see if the hit count, the dino will setjump in order to reach the high bird
+                    for(int i = 0; i < 15; i++)
+                    {
+                        dino.updateDino(env,bird,false);
+                        //env.updateEnvironment();
+                        API::refresh_window(fm);   
+                        for(double j = 0; j < 10000000; j++);
+                    }
+                    dino.setJump();
+                    for(int i = 0; i < 30; i++)
+                    {
+                        dino.updateDino(env,bird,false);
+                        //env.updateEnvironment();
+                        API::refresh_window(fm);   
+                        for(double j = 0; j < 10000000; j++);
+                    }
+                    life--;
+                }
+            }
+            fm.close();
+            if(dino.deadOrNot())
+            {
+                return true;
+            } 
+            else
+            {
+                return false;
+            }  
+        });
+        
+        fm.show();
+        nana::exec();
+        T.join();
+    }
+    catch(exception)
+    {
+        cout<<"crashed!"<<endl;
+    }
+}
+
 int main()
 {
     if(doIT::Dino_HP_Add())
